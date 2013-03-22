fb83261ef146121b5bbc3c387c163278f764b92c
Aginsun <aginsun@gmail.com>
2013-03-22 23:49:36 +0100

    Block/config helpers

--- xolova/divinerpg/utils/helpers/block/TwilightBlockHelper.java
+++ xolova/divinerpg/utils/helpers/block/TwilightBlockHelper.java
@@ -1,9 +1,60 @@
 package xolova.divinerpg.utils.helpers.block;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import xolova.divinerpg.blocks.twilight.*;
+import xolova.divinerpg.utils.helpers.config.TwilightConfigHelper;
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+
 public class TwilightBlockHelper 
 {
-	public static void init()
+	public static Block TwilightStone;
+	//-==-DRAVITE-==-//
+	public static Block DraviteGrass;
+	public static Block DraviteDirt;
+	public static Block DraviteLogs;
+	public static Block DraviteLeaves;
+	public static Block DraviteOre;
+	//-==-Azurite-==-//
+	public static Block AzuriteGrass;
+	public static Block AzuriteDirt;
+	public static Block AzuriteLogs;
+	public static Block AzuriteLeaves;
+	public static Block AzuriteOre;
+	
+	public static void initBlocks()
 	{
+		TwilightStone = new BlockTwilightStone(TwilightConfigHelper.TwilightStoneID).setUnlocalizedName("TwilightStone").setCreativeTab(CreativeTabs.tabBlock);
+
+		//DraviteGrass = new BlockDraviteGrass(TwilightConfigHelper.DraviteGrassID).setUnlocalizedName("DraviteGrass");
+		DraviteDirt = new BlockDraviteDirt(TwilightConfigHelper.DraviteDirtID).setUnlocalizedName("DraviteDirt").setCreativeTab(CreativeTabs.tabBlock);
+		DraviteLogs = new BlockTwilightWood(TwilightConfigHelper.DraviteLogsID).setUnlocalizedName("DraviteLogs").setCreativeTab(CreativeTabs.tabBlock);
+		//DraviteLeaves = new BlockTwilightLeaves(TwilightConfigHelper.DraviteLeavesID).setUnlocalizedName("DraviteLeaves");
+		DraviteOre = new BlockTwilightOre(TwilightConfigHelper.DraviteOreID).setUnlocalizedName("DraviteOre").setCreativeTab(CreativeTabs.tabBlock);
 		
+		//AzuriteGrass = new BlockAzuriteGrass(TwilightConfigHelper.AzuriteGrassID).setUnlocalizedName("AzuriteGrass");
+		//AzuriteDirt = new BlockAzuriteDirt(TwilightConfigHelper.AzuriteDirtID).setUnlocalizedName("AzuriteDirt");
+		AzuriteLogs = new BlockTwilightWood(TwilightConfigHelper.AzuriteLogsID).setUnlocalizedName("AzuriteLogs").setCreativeTab(CreativeTabs.tabBlock);
+		//AzuriteLeaves = new BlockTwilightLeaves(TwilightConfigHelper.AzuriteLeavesID).setUnlocalizedName("AzuriteLeaves");
+		AzuriteOre = new BlockTwilightOre(TwilightConfigHelper.AzuriteOreID).setUnlocalizedName("AzuriteOre").setCreativeTab(CreativeTabs.tabBlock);
+		
+		InitGameRegistryBlocks();
+	}
+	
+	public static void InitGameRegistryBlocks()
+	{
+		GameRegistry.registerBlock(TwilightStone, "TwilightStone");
+		
+		//GameRegistry.registerBlock(DraviteGrass, "DraviteGrass");
+		GameRegistry.registerBlock(DraviteDirt, "DraviteDirt");
+		GameRegistry.registerBlock(DraviteLogs, "DraviteLogs");
+		//GameRegistry.registerBlock(DraviteLeaves, "DraviteLeaves");
+		GameRegistry.registerBlock(DraviteOre, "DraviteOre");
+		
+		//GameRegistry.registerBlock(AzuriteGrass, "AzuriteGrass");
+		//GameRegistry.registerBlock(AzuriteDirt, "AzuriteDirt");
+		GameRegistry.registerBlock(AzuriteLogs, "AzuriteLogs");
+		//GameRegistry.registerBlock(AzuriteLeaves, "AzuriteLeaves");
+		GameRegistry.registerBlock(AzuriteOre, "AzuriteOre");
 	}
 }
--- xolova/divinerpg/utils/helpers/config/TwilightConfigHelper.java
+++ xolova/divinerpg/utils/helpers/config/TwilightConfigHelper.java
@@ -1,9 +1,49 @@
 package xolova.divinerpg.utils.helpers.config;
 
+import java.io.File;
+
+import net.minecraftforge.common.Configuration;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+
 public class TwilightConfigHelper 
 {
-	public static void init()
+	public static int TwilightStoneID;
+
+	public static int DraviteGrassID;
+	public static int DraviteDirtID;
+	public static int DraviteLogsID;
+	public static int DraviteLeavesID;
+	public static int DraviteOreID;
+	
+	public static int AzuriteGrassID;
+	public static int AzuriteDirtID;
+	public static int AzuriteLogsID;
+	public static int AzuriteLeavesID;
+	public static int AzuriteOreID;
+	
+	public static void initConfig(FMLPreInitializationEvent event)
 	{
+		int id = 175;
+		File file = new File(event.getModConfigurationDirectory(), "DivineRPGTwilight.cfg");
+		Configuration config = new Configuration(file);
 		
+		config.load();
+		
+		TwilightStoneID = config.getBlock("Twilight Stone", id++).getInt();
+		
+		DraviteGrassID = config.getBlock("Dravite Grass", id++).getInt();
+		DraviteDirtID = config.getBlock("Dravite Dirt", id++).getInt();
+		DraviteLogsID = config.getBlock("Dravite Logs", id++).getInt();
+		DraviteLeavesID = config.getBlock("Dravite Leaves", id++).getInt();
+		DraviteOreID = config.getBlock("Dravite Ore", id++).getInt();
+		
+		AzuriteGrassID = config.getBlock("Azurite Grass", id++).getInt();
+		AzuriteDirtID = config.getBlock("Azurite Dirt", id++).getInt();
+		AzuriteLogsID = config.getBlock("Azurite Logs", id++).getInt();
+		AzuriteLeavesID = config.getBlock("Azurite Leaves", id++).getInt();
+		AzuriteOreID = config.getBlock("Azurite Ore", id++).getInt();
+		
+		config.save();
 	}
 }

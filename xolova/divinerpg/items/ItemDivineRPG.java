408eec3ea6860ff5ad37f35264a79142ed6927de
Vazkii <vazkii@hotmail.com>
2013-03-23 22:22:30 +0000

    Fixed Items not rendering well outside of any contextes where various render passes are forced.

--- xolova/divinerpg/items/ItemDivineRPG.java
+++ xolova/divinerpg/items/ItemDivineRPG.java
@@ -33,17 +33,22 @@ public class ItemDivineRPG extends Item
 	}
 	
 	// Meant to be overrided
-	public int getIndex(int side, int metadata) {
+	public int getIndex(int meta, int pass) {
 		return index;
 	}
 	
 	// Meant to be overrided
-	public int getSheet(int side, int metadata) {
+	public int getSheet(int meta, int pass) {
 		return sheet;
+	}
+
+	@Override
+	public Icon getIconFromDamage(int par1) {
+		return IconHelper.icons[getSheet(par1, 0)][getIndex(par1, 0)];
 	}
 	
 	@Override
 	public Icon getIconFromDamageForRenderPass(int par1, int par2) {
-		return IconHelper.icons[getSheet(par1, par2)][getIndex(par1, par2)];
+		return getIconFromDamage(par1);
 	}
 }

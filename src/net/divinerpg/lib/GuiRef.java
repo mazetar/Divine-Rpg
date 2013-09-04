53093ce55abc0ce781be17d9d4b4d9acb6164e04
Mazetar <admin@mazetar.com>
2013-09-05 01:08:40 +0200

    Adds GUI Handler

---
+++ src/net/divinerpg/lib/GuiRef.java
@@ -0,0 +1,31 @@
+package net.divinerpg.lib;
+
+import net.minecraft.util.ResourceLocation;
+
+public class GuiRef {
+    static int guiID = 1;
+    public static final int CONDENSER = ++guiID;
+    public static final int TRADING = ++guiID;
+    public static final int MERIK = ++guiID;
+    public static final int DATTICON = ++guiID;
+    public static final int DEMONFURNACE = ++guiID; 
+    public static final int DREAMLAMP =  ++guiID;
+    public static final int EXTRACTOR =  ++guiID;
+    public static final int FURNACE =  ++guiID;
+    public static final int HUNGER = ++guiID;
+    public static final int INFUSIONTABLE = ++guiID;
+    public static final int JACKOMAN = ++guiID;
+    public static final int LEORNA = ++guiID;
+    public static final int MOLTENFURNACE = ++guiID;
+    public static final int ENCHANT = ++guiID;
+    public static final int ITEM_BOOK = ++guiID;
+    public static final int TINKERER = ++guiID;
+    public static final int VATTICUS = ++guiID;
+    public static final int ZELUS = ++guiID;
+    
+            
+            
+    
+}
+
+
---
+++ src/net/divinerpg/utils/handlers/GuiHandler.java
@@ -0,0 +1,137 @@
+package net.divinerpg.utils.handlers;
+
+import net.divinerpg.lib.GuiRef;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import cpw.mods.fml.common.network.IGuiHandler;
+
+public class GuiHandler implements IGuiHandler {
+
+    @Override
+    public Object getServerGuiElement(int id, EntityPlayer player, World world,
+                    int x, int y, int z) {
+        
+        if (id == GuiRef.CONDENSER) {
+            
+        }
+        else if (id == GuiRef.DATTICON) {
+            
+        }
+        else if (id == GuiRef.DEMONFURNACE) {
+            
+        }
+        else if (id == GuiRef.DREAMLAMP) {
+            
+        }
+        else if (id == GuiRef.ENCHANT) {
+            
+        }
+        else if (id == GuiRef.EXTRACTOR) {
+            
+        }
+        else if (id == GuiRef.FURNACE) {
+            
+        }
+        else if (id == GuiRef.HUNGER) {
+            
+        }
+        else if (id == GuiRef.INFUSIONTABLE) {
+            
+        }
+        else if (id == GuiRef.ITEM_BOOK) {
+            
+        }
+        else if (id == GuiRef.JACKOMAN) {
+            
+        }
+        else if (id == GuiRef.LEORNA) {
+            
+        }
+        else if (id == GuiRef.MERIK) {
+            
+        }
+        else if (id == GuiRef.MOLTENFURNACE) {
+            
+        }
+        else if (id == GuiRef.TINKERER) {
+            
+        }
+        else if (id == GuiRef.TRADING) {
+            
+        }
+        else if (id == GuiRef.VATTICUS) {
+            
+        }
+        else if (id == GuiRef.ZELUS) {
+            
+        }
+            
+            return null;
+    }
+
+    @Override
+    public Object getClientGuiElement(int id, EntityPlayer player, World world,
+                    int x, int y, int z) {
+        if (id == GuiRef.CONDENSER) {
+            
+        }
+        else if (id == GuiRef.DATTICON) {
+            
+        }
+        else if (id == GuiRef.DEMONFURNACE) {
+            
+        }
+        else if (id == GuiRef.DREAMLAMP) {
+            
+        }
+        else if (id == GuiRef.ENCHANT) {
+            
+        }
+        else if (id == GuiRef.EXTRACTOR) {
+            
+        }
+        else if (id == GuiRef.FURNACE) {
+            
+        }
+        else if (id == GuiRef.HUNGER) {
+            
+        }
+        else if (id == GuiRef.INFUSIONTABLE) {
+            
+        }
+        else if (id == GuiRef.ITEM_BOOK) {
+            
+        }
+        else if (id == GuiRef.JACKOMAN) {
+            
+        }
+        else if (id == GuiRef.LEORNA) {
+            
+        }
+        else if (id == GuiRef.MERIK) {
+            
+        }
+        else if (id == GuiRef.MOLTENFURNACE) {
+            
+        }
+        else if (id == GuiRef.TINKERER) {
+            
+        }
+        else if (id == GuiRef.TRADING) {
+            
+        }
+        else if (id == GuiRef.VATTICUS) {
+            
+        }
+        else if (id == GuiRef.ZELUS) {
+            
+        }
+                
+            
+            
+    
+        
+        return null;
+
+    }
+}

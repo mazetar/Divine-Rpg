430a8ff4cae6e173eb1e79ea94e40d2c9201fd8b
Mazetar <admin@mazetar.com>
2013-09-05 01:59:18 +0200

    Adds spawn eggs

--- src/net/divinerpg/utils/helpers/entity/ModMobRegistry.java
+++ src/net/divinerpg/utils/helpers/entity/ModMobRegistry.java
@@ -3,6 +3,8 @@ package net.divinerpg.utils.helpers.entity;
 import net.divinerpg.DivineRPG;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityEggInfo;
+import net.minecraft.entity.EntityList;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.relauncher.Side;
@@ -14,15 +16,18 @@ public class ModMobRegistry
     
 	public static void registerModEntity(Class<? extends Entity> clazz, String name, int modID, int trackingRange, int updateFrequency, boolean sendsVelocityUpdates) {
 		EntityRegistry.registerModEntity(clazz, name, NEXT_MOD_ENTITY_ID++, DivineRPG.instance, trackingRange, updateFrequency, sendsVelocityUpdates);
+        registerEgg(NEXT_MOD_ENTITY_ID, clazz);
 	}
 	
 	public static void registerEntity(Class<? extends Entity> clazz, String name, int modID, int trackingRange, int updateFrequency, boolean sendsVelocityUpdates) {
 		EntityRegistry.registerModEntity(clazz, name, NEXT_MOD_ENTITY_ID++, DivineRPG.instance, trackingRange, updateFrequency, sendsVelocityUpdates);
+        registerEgg(NEXT_MOD_ENTITY_ID, clazz);
 	}
 	
 	public static void registerEntity(Class<? extends Entity> clazz, String name, int modID, int trackingRange, int updateFrequency, boolean sendsVelocityUpdates, int eggColor1, int eggColor2) 
 	{
 		EntityRegistry.registerModEntity(clazz, name, NEXT_MOD_ENTITY_ID++, DivineRPG.instance, trackingRange, updateFrequency, sendsVelocityUpdates);
+        registerEntityEgg(NEXT_MOD_ENTITY_ID, clazz, eggColor1, eggColor2);
 	}
 	
 	public static int availableID() {
@@ -35,20 +40,31 @@ public class ModMobRegistry
 		RenderingRegistry.registerEntityRenderingHandler(clazz, render);
 	}
 	
-	   public static void registerModEntity(Class<? extends Entity> clazz, String name, int trackingRange, int updateFrequency, boolean sendsVelocityUpdates) {
-	        EntityRegistry.registerModEntity(clazz, name, NEXT_MOD_ENTITY_ID++, DivineRPG.instance, trackingRange, updateFrequency, sendsVelocityUpdates);
-	   }
-	    
-	    public static void registerEntity(Class<? extends Entity> clazz, String name, int trackingRange, int updateFrequency, boolean sendsVelocityUpdates) {
-	        EntityRegistry.registerModEntity(clazz, name, NEXT_MOD_ENTITY_ID++, DivineRPG.instance, trackingRange, updateFrequency, sendsVelocityUpdates);
-	    }
-	    
-	    public static void registerEntity(Class<? extends Entity> clazz, String name, int trackingRange, int updateFrequency, boolean sendsVelocityUpdates, int eggColor1, int eggColor2) 
-	    {
-	        EntityRegistry.registerModEntity(clazz, name, NEXT_MOD_ENTITY_ID++, DivineRPG.instance, trackingRange, updateFrequency, sendsVelocityUpdates);
-	    }
-	
-	
+   public static void registerModEntity(Class<? extends Entity> clazz, String name, int trackingRange, int updateFrequency, boolean sendsVelocityUpdates) {
+        EntityRegistry.registerModEntity(clazz, name, NEXT_MOD_ENTITY_ID++, DivineRPG.instance, trackingRange, updateFrequency, sendsVelocityUpdates);
+        registerEgg(NEXT_MOD_ENTITY_ID, clazz);
+   }
+    
+    public static void registerEntity(Class<? extends Entity> clazz, String name, int trackingRange, int updateFrequency, boolean sendsVelocityUpdates) {
+        EntityRegistry.registerModEntity(clazz, name, NEXT_MOD_ENTITY_ID++, DivineRPG.instance, trackingRange, updateFrequency, sendsVelocityUpdates);
+       registerEgg(NEXT_MOD_ENTITY_ID, clazz);
+    }
+    
+    public static void registerEntity(Class<? extends Entity> clazz, String name, int trackingRange, int updateFrequency, boolean sendsVelocityUpdates, int eggColor1, int eggColor2) 
+    {
+        EntityRegistry.registerModEntity(clazz, name, NEXT_MOD_ENTITY_ID++, DivineRPG.instance, trackingRange, updateFrequency, sendsVelocityUpdates);
+        registerEntityEgg(NEXT_MOD_ENTITY_ID, clazz, eggColor1, eggColor2);
+    }
+    
+    private static void registerEgg(int id, Class<? extends Entity> entity) {
+        registerEntityEgg(id, entity, 0x333333, 0x990099);
+    }
+    
+    public static void registerEntityEgg(int id, Class<? extends Entity> entity, int primaryColor, int secondaryColor) 
+    {
+         EntityList.IDtoClassMapping.put(id, entity);
+         EntityList.entityEggs.put(id, new EntityEggInfo(id, primaryColor, secondaryColor));
+    }
 }
 
 /* EntityRegistry.registerModEntity */

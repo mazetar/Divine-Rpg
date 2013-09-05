37434c5b8178fbb38d2ef1095efa382ed2171d0d
Mazetar <admin@mazetar.com>
2013-09-05 18:50:19 +0200

    Adds TileEntity registration to Vethea

--- src/net/divinerpg/utils/proxies/VetheaProxy.java
+++ src/net/divinerpg/utils/proxies/VetheaProxy.java
@@ -1,12 +1,15 @@
 package net.divinerpg.utils.proxies;
 
+import net.divinerpg.blocks.vethea.tileentities.*;
 import net.divinerpg.utils.helpers.entity.VetheaEntityHelper;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class VetheaProxy {
 	
     public void preInit(FMLPreInitializationEvent event) {
+        registerTileEntities();
     }
     
     public void init(FMLInitializationEvent event) {
@@ -16,5 +19,9 @@ public class VetheaProxy {
     public void initEntityRenderers() {
         VetheaEntityHelper.init();
     }
-
+    
+    void registerTileEntities() {
+        GameRegistry.registerTileEntity(TileEntityDreamLamp.class, "Dream Lamp");
+        GameRegistry.registerTileEntity(TileEntityInfusionTable.class, "Infusion Table");
+    }
 }
